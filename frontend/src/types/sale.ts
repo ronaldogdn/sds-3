340f5938941b4de86b3c59e00113b26ce5e19f83
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-05-24 20:09:24 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -2,6 +2,12 @@
  * Cria o tipo sale sum
  */
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
