895f456e9cc78f742a21af60279433087afa204e
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-05-24 21:06:11 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,26 @@
-/**
- * Cria o tipo sale sum
- */
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
